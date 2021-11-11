e6daba6c377d1f75958aa664949efe948dcbf65a
lissptz <80944518+lissptz@users.noreply.github.com>
2021-11-11 23:58:49 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,79 @@
+const
+ prev = document.getElementById("btn-prev"),
+ next = document.getElementById("btn-next"),
+ slides=document.querySelectorAll(".slide"),
+ dots=document.querySelectorAll(".dot");
+
+ let index=0;
+ let timer = 0;
+
+ const activeSlide = n =>{
+     for ( slide of slides ) {
+           slide.classList.remove("active");
+        
+
+
+     }
+     slides[n].classList.add("active");
+    }
+    const activeDot = n =>{
+        for ( dot of dots ) {
+              dot.classList.remove("active");
+            
+   
+        }
+        dots[n].classList.add("active");
+       }
+       const prepareCurrentSlide = ind =>{
+        activeSlide(index);
+        activeDot(index);
+        
+       }
+
+ const nextSlide= () => {
+if (index == slides.length - 1) {
+    index = 0;
+    prepareCurrentSlide(index);
+} else{
+index++;
+prepareCurrentSlide(index);
+
+
+}
+ }
+ const prevSlide = () =>{
+    if (index == 0) {
+        index = slides.length -1;
+        prepareCurrentSlide(index);
+    } else{
+    index--;
+    prepareCurrentSlide(index);
+    }
+     
+
+}
+dots.forEach((item,indexDot)=>{
+    item.addEventListener("click",()=>{
+        index=indexDot;
+        prepareCurrentSlide(index);
+    })
+
+})
+ next.addEventListener("click",nextSlide);
+ prev.addEventListener("click",prevSlide);
+
+function makeTimer(){
+    nextSlide();
+}
+setInterval(makeTimer,2000);
+
+
+//  function makeTimer(){
+//     clearInterval(timer) 
+   
+//     timer = setInterval(function(){
+//      index++;
+//      prepareCurrentSli(index);
+//      },2000);
+//   }
+//   makeTimer(); 
